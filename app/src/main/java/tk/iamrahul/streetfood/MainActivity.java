cea168ef1f9f2a0610a615b8118a0696514697cc
DESKTOP-59G3BMH\user <rahul3rde@gmail.com>
2017-06-05 12:05:28 +0530

    gender, email & name done

--- app/src/main/java/tk/iamrahul/streetfood/MainActivity.java
+++ app/src/main/java/tk/iamrahul/streetfood/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,11 +12,16 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -46,12 +52,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
 
+
                 String user_id = loginResult.getAccessToken().getUserId();
                 txt.setText("Fb token"+loginResult.getAccessToken().getUserId());
                 Picasso.with(context)
                         .load("https://graph.facebook.com/" + user_id+ "/picture?type=large")
                         .into(imageview);
 
+
+
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.v("LoginActivity", response.toString());
+
+                                // Application code
+                                try {
+                                    String email = object.getString("email");
+                                    String gender = object.getString("gender");
+                                    String name = object.getString("name");
+                                    //String birthday = object.getString("birthday"); // 01/31/1980 format
+
+                                    txt.setText(email+name+gender);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
             }
 
             @Override
@@ -65,6 +99,9 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        LoginManager.getInstance().logOut();
 
     }
 
